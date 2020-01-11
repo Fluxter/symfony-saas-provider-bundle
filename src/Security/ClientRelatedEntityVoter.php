bdeece393ce5a0301011b098571ac13da0d6bf66
Marcel <mk@fluxter.net>
2020-01-11 20:21:16 +0100

    added permissionsvoter

---
+++ src/Security/ClientRelatedEntityVoter.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the ClanManager package.
+ * (c) Fluxter <https://fluxter.net/>
+ * Found us at <https://clanmanager.net>
+ */
+
+namespace Fluxter\SaasProviderBundle\Security;
+
+use Fluxter\SaasProviderBundle\Model\SaasClientRelatedInterface;
+use Fluxter\SaasProviderBundle\Service\SaasClientService;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+
+class ClientRelatedEntityVoter extends Voter
+{
+    private SaasClientService $_clientService;
+
+    public function __construct(SaasClientService $clientService)
+    {
+        $this->_clientService = $clientService;
+    }
+
+    protected function supports($attribute, $subject)
+    {
+        if (!$subject instanceof SaasClientRelatedInterface) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param string                     $attribute
+     * @param SaasClientRelatedInterface $subject
+     *
+     * @return void
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        if (!$this->_clientService->getCurrentClient()) {
+            return false;
+        }
+
+        if ($this->_clientService->getCurrentClient() == $subject->getClient()) {
+            return true;
+        }
+
+        return false;
+    }
+}
