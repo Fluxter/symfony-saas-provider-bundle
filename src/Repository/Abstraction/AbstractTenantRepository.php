1467c086146df234a903c9ffba17688930bd1f99
Marcel Kallen <1263478-MKHSKY@users.noreply.gitlab.com>
2021-12-01 22:21:53 +0100

    Urgent fixes for not filtering the repository

--- src/Repository/Abstraction/AbstractTenantRepository.php
+++ src/Repository/Abstraction/AbstractTenantRepository.php
@@ -7,8 +7,38 @@
 namespace Fluxter\SaasProviderBundle\Repository\Abstraction;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Fluxter\SaasProviderBundle\Model\TenantInterface;
+use Fluxter\SaasProviderBundle\Service\TenantService;
 
 abstract class AbstractTenantRepository extends ServiceEntityRepository
 {
-    use TenantRepositoryTrait;
+    protected TenantService $clientService;
+
+    public function findAll()
+    {
+        return $this->createQueryBuilder('e')->getQuery()->getResult();
+    }
+
+    /** @required */
+    public function setTenantService(TenantService $clientService)
+    {
+        $this->clientService = $clientService;
+    }
+
+    public function createQueryBuilder($alias, $indexBy = null, ?TenantInterface $tenant = null)
+    {
+        if (null == $tenant) {
+            $tenant = $this->clientService->getTenant();
+        }
+
+        return parent::createQueryBuilder($alias, $indexBy)
+            ->andWhere($alias . '.tenant = :saasTenantId')
+            ->setParameter('saasTenantId', $tenant->getId());
+    }
+
+    // THIS IS ONLY FOR CONSOLE COMMANDS OR SOMETHING!
+    public function createGlobalQueryBuilder($alias, $indexBy = null)
+    {
+        return parent::createGlobalQueryBuilder($alias, $indexBy);
+    }
 }
